614f661b8c53b0000d5b8e3c3ce360981d88d8b5
jinjiacun123 <jinjiacun123@github.com>
2016-02-16 09:54:22 +0800

    书架文件重命名

---
+++ src/com/jim/magazine/BookselfFragment.java
@@ -0,0 +1,19 @@
+package com.jim.magazine;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class BookselfFragment extends Fragment implements OnClickListener {
+
+	public BookselfFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
