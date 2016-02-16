a55cc9164f99c8adc0ef6255b7f343ced0cf4bfa
jinjiacun123 <jinjiacun123@github.com>
2016-02-16 09:54:44 +0800

    圈子文件重命名

---
+++ src/com/jim/magazine/GroupFragment.java
@@ -0,0 +1,19 @@
+package com.jim.magazine;
+
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class GroupFragment extends Fragment implements OnClickListener {
+
+	public GroupFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
